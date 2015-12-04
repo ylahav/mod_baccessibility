7f32dbb32b49241601f4c987a5aa84b197d4086c
yair lahav <yair@billrun.net>
2015-12-04 13:41:28 +0200

    improve languages support by adding more strings in language files

--- tmpl/default.php
+++ tmpl/default.php
@@ -15,7 +15,7 @@ defined('_JEXEC') or die;
         } else {
     ?>
 	<button tabindex="0" class="b-acc_hide_toolbar $params['toolbar_position']; b-acc-icon-<?php echo $params['icon_size']; ?>">
-		<span>Accessibility</span>
+		<span><?php echo JText::_('MOD_BACCESSIBILITY_ACCESSIBILITY'); ?></span>
 	</button>
 	<?php
         }
@@ -25,21 +25,21 @@ defined('_JEXEC') or die;
             <h3 tabindex="0"><?php echo $module->title; ?></h3>
         <?php endif; ?>
         <li id="b-acc-fontsizer" data-size-tags="<?php echo $params['fontsizer_tags']; ?>" data-size-jump="<?php echo $params['fontsizer_size']; ?>">
-			<button class="small-letter" tabindex="0"><i class="fa fa-search-minus"></i> Decrease font size</button>
-			<button class="big-letter" tabindex="0"><i class="fa fa-search-plus"></i> Increase font size</button>
-			<button class="b-acc-font-reset b-acc-hide" tabindex="0"><i class="fa fa-refresh"></i> Default font sizes</button>
+			<button class="small-letter" tabindex="0"><i class="fa fa-search-minus"></i> <?php echo JText::_('MOD_BACCESSIBILITY_DECREASE_FONT_SIZE'); ?></button>
+			<button class="big-letter" tabindex="0"><i class="fa fa-search-plus"></i> <?php echo JText::_('MOD_BACCESSIBILITY_INCREASE_FONT_SIZE'); ?></button>
+			<button class="b-acc-font-reset b-acc-hide" tabindex="0"><i class="fa fa-refresh"></i> <?php echo JText::_('MOD_BACCESSIBILITY_DEFAULT_FONT_SIZES'); ?></button>
 		</li>
 		<li id="b-acc-contrast">
-			<button class="b-acc-bright-btn" tabindex="0"><i class="fa fa-sun-o"></i> Bright contrast</button>
-			<button class="b-acc-dark-btn" tabindex="0"><i class="fa fa-moon-o"></i> Dark contrast</button>
-			<button class="b-acc-grayscale" tabindex="0"><i class="fa fa-gg-circle"></i> Grayscale</button>
-            <button class="b-acc-contrast-reset" tabindex="0"><i class="fa fa-refresh"></i> Reset contrast</button>
+			<button class="b-acc-bright-btn" tabindex="0"><i class="fa fa-sun-o"></i> <?php echo JText::_('MOD_BACCESSIBILITY_BRIGHT_CONTRAST'); ?></button>
+			<button class="b-acc-dark-btn" tabindex="0"><i class="fa fa-moon-o"></i> <?php echo JText::_('MOD_BACCESSIBILITY_DARK_CONTRAST'); ?></button>
+			<button class="b-acc-grayscale" tabindex="0"><i class="fa fa-gg-circle"></i> <?php echo JText::_('MOD_BACCESSIBILITY_GRAYSCALE'); ?></button>
+            <button class="b-acc-contrast-reset" tabindex="0"><i class="fa fa-refresh"></i> <?php echo JText::_('MOD_BACCESSIBILITY_RESET_CONTRAST'); ?></button>
 		</li>
 		<li id="b-acc-keyboard-navigation">
-			<button id="b-acc-keyboard" tabindex="0"><i class="fa fa-keyboard-o"></i> Keyboard Navigation</button>
+			<button id="b-acc-keyboard" tabindex="0"><i class="fa fa-keyboard-o"></i> <?php echo JText::_('MOD_BACCESSIBILITY_KEYBOARD_NAVIGATION'); ?></button>
 		</li>
 		<li id="b-acc-links">
-			<button class="b-acc-toggle-underline" tabindex="0"><i class="fa fa-underline"></i> Toggle underline</button>
+			<button class="b-acc-toggle-underline" tabindex="0"><i class="fa fa-underline"></i> <?php echo JText::_('MOD_BACCESSIBILITY_TOGGLE_UNDERLINE'); ?></button>
 		</li>
 	</ul>
 </nav>
